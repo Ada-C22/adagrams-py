0a600dd85eb101adbb28bf2ff2240774fb675a54
AstryBrana <astry.brana@gmail.com>
2024-09-13 12:59:55 -0400

    wave 1 completed with tests passing

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,6 +1,50 @@
-def draw_letters():
-    pass
+import random
 
+def draw_letters():
+    LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+    }
+
+    weighted_list = []
+    
+    for letter, amount in LETTER_POOL.items():
+	    weighted_list += letter * amount
+
+    letters = []
+    
+    while len(letters) < 10:
+        random_index = random.randrange(0,len(weighted_list))
+        letters += weighted_list[random_index]
+        weighted_list.pop(random_index)
+        
+
+    return letters
+    
 def uses_available_letters(word, letter_bank):
     pass
 
