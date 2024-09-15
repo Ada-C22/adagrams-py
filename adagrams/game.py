be848976e63b6abe2ad6e1816d6ba74266cf464f
Christelle Nkera <scnkera@gmail.com>
2024-09-15 03:41:41 -0400

    Updated python file

--- adagrams/game.py
+++ adagrams/game.py
@@ -42,7 +42,26 @@ def uses_available_letters(word, letter_bank):
     return True
 
 def score_word(word):
-    pass
+    points_dict = {
+    'A': 1, 'B': 3, 'C': 3, 'D': 2, 'E': 1, 'F': 4, 'G': 2, 'H': 4, 
+    'I': 1, 'J': 8, 'K': 5, 'L': 1, 'M': 3, 'N': 1, 'O': 1, 'P': 3, 
+    'Q': 10, 'R': 1, 'S': 1, 'T': 1, 'U': 1, 'V': 4, 'W': 4, 'X': 8, 
+    'Y': 4, 'Z': 10
+    }
+    word = word.upper()
+    BONUS_POINTS = 8
+    total_points = 0
+
+    for input_letter in word:
+        total_points += points_dict[input_letter]
+
+    if len(word) >= 7:
+        total_points += BONUS_POINTS
+
+    return total_points
+
+score_word("XXXXXXX")
+
 
 def get_highest_word_score(word_list):
     pass
