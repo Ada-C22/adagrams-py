0c0c067608832d91920f99ca5aff39ec166c9745
Weixi He <heweixiguge@gmai.com>
2024-09-14 22:02:06 -0400

    first part wave 1

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,43 @@
+import random
+import pytest
+
 def draw_letters():
-    pass
+    LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+}
+    # build a hand_letter_list
+    letter_list = []
+    # turning LETTER_POOL dictionary into a list
+    for letter, count in LETTER_POOL.items():
+        letter_list += [letter] * count
+    # reture a hand of letters list
+    return random.sample(letter_list, 10)
+
 
 def uses_available_letters(word, letter_bank):
     pass
