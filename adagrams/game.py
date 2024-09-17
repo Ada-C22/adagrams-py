52d69b7a076f3312f920c453b7d8541d5bfeecaf
Amber <110812972+Msambere@users.noreply.github.com>
2024-09-17 17:54:44 -0500

    Refactor code
    Changed draw_hand() to take into account the differences in probability
    for drawing each letter. Changed variable names to be more descriptive.
    Added comments. Condensed code by removing unneccessary lines.

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,4 @@
 import random
-import string
 
 LETTER_POOL = {
     'A': 9, 
@@ -30,56 +29,68 @@ LETTER_POOL = {
     'Z': 1
 }
 
+
+
+
 def draw_letters():
-    hand =[]
-    letter_frequncies={}
-    while len(hand)<10:
-        # random_letter = random.choice(string.ascii_letters).upper()
-        alphabet = list(LETTER_POOL.keys())
-        random_index = random.randint(0,25)
-        random_letter = alphabet[random_index]
-        if random_letter not in hand:
-            hand.append(random_letter)
-            letter_frequncies[random_letter]=1
-        else:
-            if letter_frequncies[random_letter]< LETTER_POOL[random_letter]:
-                hand.append(random_letter)
-                letter_frequncies[random_letter] +=1
-            else: 
-                continue
+    # Create empty list to store choosen letters
+    hand = []
+    # Create a complete pool of letter options
+    weighted_letter_pool = []
+    for letter, freq in LETTER_POOL.items():
+        # Add the letter to the pool as many times as its frequency value
+        for _ in range(freq):
+            weighted_letter_pool.append(letter)
+    # Randonly choose letters for hand ()
+    while len(hand) < 10:
+        random_index = random.randint(0, len(weighted_letter_pool)-1)
+        random_letter = weighted_letter_pool[random_index]
+        # Add letter to hand
+        hand.append(random_letter)
+        # Remove picked letter from pool
+        weighted_letter_pool.pop(random_index)
     return hand
 
 def uses_available_letters(word, letter_bank):
-    letter_list = list(word.upper())
-    letter_bank_freqs={}
-    letter_list_freqs={}
+    # Change word to uppercase
+    word = word.upper()
+    # Create a dictionary of the frequency of letters in letter_bank
+    max_letter_freqs = {}
     for letter in letter_bank:
-            if letter not in letter_bank_freqs:
-                letter_bank_freqs[letter] = 1
+            if letter not in max_letter_freqs:
+                max_letter_freqs[letter] = 1
             else:
-                letter_bank_freqs[letter] += 1
-    for letter in letter_list:
+                max_letter_freqs[letter] += 1
+    # Create dictionary to track the number of times a lettr is used in a word
+    letter_use_freqs = {}
+    # Check if the word is in the letter_bank
+    for letter in word:
         if letter not in letter_bank:
             return False
+        # Check if letter is in letter_use_freqs
+        if letter not in letter_use_freqs:
+            # Create entry if not
+            letter_use_freqs[letter] = 1
         else:
-            if letter not in letter_list_freqs:
-                letter_list_freqs[letter] = 1
+            # If the letter usage hasn't reached it's maximum frequncy, allow it
+            if letter_use_freqs[letter]< max_letter_freqs[letter]:
+                letter_use_freqs[letter] += 1
             else:
-                if letter_list_freqs[letter]>= letter_bank_freqs[letter]:
-                    return False
-                else:
-                    letter_list_freqs[letter] += 1
+                # if the letter has already reached it's maximum frequency, return False
+                return False
 
     return True
 
 def score_word(word):
+    # Create score tracking variable
     score = 0
+    # Add the corresponding score for each letter
     for letter in word:
         letter = letter.upper()
         if letter in ["A","E","I", "O", "U", "L", "N", "R","S","T"]:
             score += 1
         elif letter in ["D","G"]:
-            score +=2
+            score += 2
         elif letter in ["B","C","M","P"]:
             score += 3
         elif letter in ["F","H","V","W","Y"]:
@@ -90,23 +101,31 @@ def score_word(word):
             score += 8
         elif letter in ["Q","Z"]:
             score += 10
-    if len(word)>= 7:
+    # Add 8 point bonus for words longer than 7 letters
+    if len(word) >= 7:
         score += 8
     return score
 
 def get_highest_word_score(word_list):
-    current_highest = [word_list[0], score_word(word_list[0])]
+    # Create a variable to hold the word with hightest score
+    highest_score = {"word": "", "score":0}
+    # Iterate throught the word_list
     for word in word_list:
-        current_word_score= score_word(word)
-        if current_word_score > current_highest[1] and len(current_highest[0])<10:
-            current_highest[0] = word
-            current_highest [1] = current_word_score 
-        elif current_word_score == current_highest[1]:
-            if len(current_highest[0]) == 10:
+        # Score the word
+        current_word_score = score_word(word)
+        # Replace highest_score values  if the current_word_score is higher
+        if current_word_score > highest_score["score"]:
+            highest_score["word"] = word
+            highest_score ["score"] = current_word_score 
+        # Tie-breakers
+        elif current_word_score == highest_score["score"]:
+            # Keep the highest_score if it is ten letters long
+            if len(highest_score["word"]) == 10:
                 continue
-            elif len(word)==10 or len(word)< len(current_highest[0]):
-                current_highest[0] = word
-                current_highest [1] = current_word_score
-    return tuple(current_highest)
+            # Replace highest_score if the current_word is ten letters or shorter than the highest_score 
+            elif len(word) == 10 or len(word) < len(highest_score["word"]):
+                highest_score["word"] = word
+                highest_score ["score"] = current_word_score
+    return tuple([highest_score["word"], highest_score["score"]])
     
 
