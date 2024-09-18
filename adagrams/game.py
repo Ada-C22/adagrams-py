0905e79b07ec5156a26c4fdf18f5a2f0d9e09735
Liubov Davidova <lubasha0512@gmail.com>
2024-09-18 12:16:40 -0700

    Implemented function score_word

--- adagrams/game.py
+++ adagrams/game.py
@@ -65,7 +65,45 @@ def uses_available_letters(word, letter_bank):
 
 
 def score_word(word):
-    pass
+    word_upper = word.upper()
+    score = 0
+
+    if len(word_upper) >= 7 and len(word_upper) <= 10:
+        score += 8
+    
+    score_chart_dict = {}
+    score_chart_dict["A"] = 1
+    score_chart_dict["E"] = 1
+    score_chart_dict["I"] = 1
+    score_chart_dict["O"] = 1
+    score_chart_dict["U"] = 1
+    score_chart_dict["L"] = 1
+    score_chart_dict["N"] = 1
+    score_chart_dict["R"] = 1
+    score_chart_dict["S"] = 1
+    score_chart_dict["T"] = 1
+    score_chart_dict["D"] = 2
+    score_chart_dict["G"] = 2
+    score_chart_dict["B"] = 3
+    score_chart_dict["C"] = 3
+    score_chart_dict["M"] = 3
+    score_chart_dict["P"] = 3
+    score_chart_dict["F"] = 4
+    score_chart_dict["H"] = 4
+    score_chart_dict["V"] = 4
+    score_chart_dict["W"] = 4
+    score_chart_dict["Y"] = 4
+    score_chart_dict["K"] = 5
+    score_chart_dict["J"] = 8
+    score_chart_dict["X"] = 8
+    score_chart_dict["Q"] = 10
+    score_chart_dict["Z"] = 10
+
+    for letter in word_upper:
+        score += score_chart_dict[letter]
+
+    return score
+
 
 def get_highest_word_score(word_list):
     pass
