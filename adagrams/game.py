a3ee1bebd1bd80a9d32fc8ad42a50eab89bd65df
Weixi He <heweixiguge@gmai.com>
2024-09-17 23:38:42 -0400

    finish wave3 and wave4

--- adagrams/game.py
+++ adagrams/game.py
@@ -35,22 +35,28 @@ def draw_letters():
     # turning LETTER_POOL dictionary into a list
     for letter, count in LETTER_POOL.items():
         letter_list += [letter] * count
-    # reture a hand of letters list
-    return random.sample(letter_list, 10)
+    # build a hand of letters list
+    hand_card = []
+    for i in range(10):
+        index = random.randint(0,len(letter_list)-1)
+        hand_card.append(letter_list[index])
+        letter_list.pop(index) 
+
+    return hand_card
 
 
 def uses_available_letters(word, letter_bank):
     word = word.upper()
     word_count = {}
     letter_bank_count = {}
-    # count how many string in word
+    # count how many string in word and build dict
     for i in word:
         if i in word_count:
             word_count[i] += 1
         else:
             word_count[i] = 1
 
-    # count how many string in letter_bank
+    # count how many string in letter_bank and build dict
     for y in letter_bank:
         if y in letter_bank_count:
             letter_bank_count[y] += 1
@@ -68,11 +74,54 @@ def uses_available_letters(word, letter_bank):
 
 def score_word(word):
     # bulid dict of score
+    score_dict = {
+        'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
+        'D': 2, 'G': 2,
+        'B': 3, 'C': 3, 'M': 3, 'P': 3,
+        'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+        'K': 5,
+        'J': 8, 'X': 8,
+        'Q': 10, 'Z': 10
+    }
+    # input is empty
+    if word == "":
+        return 0
     # check each vlaue of letter
+    word = word.upper()
+    score_count = []
+    for letter in word:
+        score = score_dict.get(letter,0)
+        score_count.append(score)
+    total_score = sum(score_count)
+
+    if 7 <= len(word) <= 10:
+        total_score += 8
+
+    return total_score 
+
+
     # more than 7, 8, 9, or 10 gets an additional 8 points
     # return total score
-    pass
+
 
 
 def get_highest_word_score(word_list):
-    pass
+    # defind
+    max_score = 0
+    word = None
+    # get the highest score
+    for i in word_list:
+        score = score_word(i)
+        if score > max_score:
+            word = i
+            max_score = score
+
+        # same score compare
+        elif score == max_score:
+            if len(i) == 10 and len(word) != 10:
+                word = i
+            elif len(i) < len(word) and len(i) != 10 and len(word) != 10:
+                word = i
+
+    # return tuple
+    return (word, max_score)
