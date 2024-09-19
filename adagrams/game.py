b892542167305b7109e9610a55bcf5b9bded5f8a
Anees Quateja <aneesq87@gmail.com>
2024-09-18 19:26:00 -0700

    my changes on sep 18 7:26pm

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,11 +1,26 @@
+
+
 def draw_letters():
-    pass
+    # array_of_strings = [ ]
+    player = []
+    for letters in draw_letters():
+        player = [draw_letters(letters)]
+    return player
 
-def uses_available_letters(word, letter_bank):
-    pass
+# def uses_available_letters(word, letter_bank):
+    word = word.upper()
+    letter_bank_remaining = letter_bank.copy()
 
-def score_word(word):
-    pass
+    for letter in word:
+        if letter in letter_bank_remaining:
+            letter_bank_remaining.remove(letter)
+        else:
+            return False
+    return True
+    
 
-def get_highest_word_score(word_list):
-    pass
+# def score_word(word):
+#     pass
+
+# def get_highest_word_score(word_list):
+#     pass
