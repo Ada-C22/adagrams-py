2858c79cab5e927c604fd05d9e7d1bf88ade0b7e
nelasunitha <80846358+nelasunitha@users.noreply.github.com>
2024-09-23 23:36:07 -0700

    changes implemented per feedback

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,6 +1,7 @@
 import random
+
 def draw_letters():
-   letter_pool = {
+   Letter_POOL = {
     'A': 9,
     'B': 2,
     'C': 2,
@@ -30,36 +31,22 @@ def draw_letters():
      }
    #taking an empty list to store drawn letters
    drawn_letters =[]
-# using random.choice
-#    while len(drawn_letters) < 10:
+   total_letters = sum(Letter_POOL.values())
 
-#     #extracting all the keys from dictionary
-#     letter_pool_keys = letter_pool.keys()
-#     letter = random.choice(list(letter_pool_keys))
-#     if letter_pool[letter] > 0:
-#         drawn_letters.append(letter)
-
-#         #adding random letter to the list and decreasing the frequency of the letter
-#         letter_pool[letter]-= 1
-#    return drawn_letter
-
-
-   total_letters = sum(letter_pool.values())
-
-   while len(drawn_letters) < 10:
+   HAND_SIZE = 10
+   while len(drawn_letters) < HAND_SIZE:
         # Generating a random number between 1 and the total number of letters
-        rand_num = random.randint(1, total_letters)
+        rand_index = random.randint(1, total_letters)
 
         # Finding  the corresponding letter
         cumulative_count = 0
-        for letter, frequency in letter_pool.items():
+        for letter, frequency in Letter_POOL.items():
             cumulative_count += frequency
-            if rand_num <= cumulative_count:
+            if rand_index <= cumulative_count:
                 drawn_letters.append(letter)
-                letter_pool[letter] -= 1
+                Letter_POOL[letter] -= 1
                 total_letters -= 1
                 break
-
 
    return drawn_letters
 
@@ -69,45 +56,23 @@ def uses_available_letters(word, letter_bank):
 #    converting all the letters in letter bank to upper case
     letter_bank = [letter.upper() for letter in letter_bank]
 # making a dictionary for a letter_bank to check the frequency of letters
-    lookup = {}
+    letter_frequency_dict = {}
     for letter in letter_bank:
-        if letter in lookup:
-            lookup[letter] += 1
+        if letter in letter_frequency_dict:
+            letter_frequency_dict[letter] += 1
         else:
-            lookup[letter] = 1
+            letter_frequency_dict[letter] = 1
     #converting all the letters of word into uppercase and checking the frequebcy of the letter, if found decrementing the value or else returning false
 
     for letter in word.upper():
-        if letter not in lookup or lookup[letter] == 0:
+        if letter not in letter_frequency_dict or letter_frequency_dict[letter] == 0:
             return False
         else:
-            lookup[letter] -= 1
+            letter_frequency_dict[letter] -= 1
     return True
 
-#Approaching as per draw_letters function with nested loops
-
-# def score_word(word):
-# #    converting all the letters in letter bank to upper case
-#     word = [letter.upper() for letter in word]
-#     total_score = 0
-#     # making a dictionary with letters and it's score value
-#     score_value = {
-#         'AEIOULNRST': 1,'DG': 2, 'BCMP': 3, 'FHVWY': 4, 'K': 5, 'JX': 8, 'QZ': 10
-#         }
-
-#     for letter in word:
-#         for scoring_letter_group, score in score_value.items():
-#             if letter in scoring_letter_group:
-#                 total_score+= score
-#     if len(word) > 6 and len(word) <=10:
-#         total_score += 8
-#     return total_score
-
-
-
-# optimal solution where dictionary is created for individual letter and score value without nested loops
 def score_word(word):
-    score_value = {
+    LETTER_SCORES = {
         'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'R': 1, 'S': 1, 'T': 1,
         'D': 2, 'G': 2,
         'B': 3, 'C': 3, 'M': 3, 'P': 3,
@@ -119,18 +84,21 @@ def score_word(word):
     word = word.upper()
     total_score = 0
 
-    total_score+= sum(score_value.get(letter, 0) for letter in word )
+    total_score += sum(LETTER_SCORES.get(letter, 0) for letter in word )
 
-    if len(word) > 6 and len(word) <=10:
-        total_score += 8
+    BONUS_LENGTH_MIN = 7
+    BONUS_LENGTH_MAX = 10
+    BONUS_POINTS = 8
+
+    if BONUS_LENGTH_MIN <= len(word) <= BONUS_LENGTH_MAX:
+        total_score += BONUS_POINTS
 
     return total_score
 
 
-
 def get_highest_word_score(word_list):
     max_word = word_list[0]
-    max_word_score =score_word(max_word)
+    max_word_score = score_word(max_word)
     #iterating from the second word
     for word in word_list[1:]:
         word_score = score_word(word)
@@ -147,4 +115,4 @@ def get_highest_word_score(word_list):
                 max_word = word
 
         max_word_score = max(max_word_score, word_score)
-    return (max_word,max_word_score )
+    return (max_word, max_word_score )
