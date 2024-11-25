67edef73699e9350f3f5e7509e78da2f2a57bca9
lioliadoc <liolia.doc@gmail.com>
2024-11-24 19:32:34 -0600

    refractored Wave_1

--- adagrams/game.py
+++ adagrams/game.py
@@ -28,20 +28,22 @@ LETTER_POOL = {
     'Z': 1
 }
 
+
 def draw_letters():
     
     hand_of_letters = []
-    letter_count =  {letter: 0 for letter in LETTER_POOL}
-    letter_list = list(LETTER_POOL.keys())
+    available_letters_list = []
+    for letter, count in LETTER_POOL.items():
+        for _ in range(count):
+            available_letters_list.append(letter)
+
     while len(hand_of_letters) < 10: 
         
-        letter_index = random.randint(0, len(letter_list) - 1)
-        letter = letter_list[letter_index]
-        
-        if letter_count[letter] < LETTER_POOL[letter]:
-            hand_of_letters.append(letter)
-            letter_count[letter] += 1
-
+        letter_index = random.randint(0, len(available_letters_list) - 1)
+        letter = available_letters_list[letter_index]
+        hand_of_letters.append(letter)
+        available_letters_list.pop(letter_index)
+       
     return hand_of_letters
 
 
@@ -60,27 +62,65 @@ def uses_available_letters(word, letter_bank):
     return True
 
 
-          
 def score_word(word):
-    scores = { 
-        1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
-        2: ["D", "G" ], 
-        3: ["B", "C", "M", "P" ], 
-        4: ["F", "H", "V", "W", "Y" ], 
-        5: ["K"], 
-        8: ["J", "X"],
-        10: ["Q", "Z" ] 
+    pass
+    scores = {'A': 1, 
+            'B': 3, 
+            'C': 3, 
+            'D': 2,
+            'E': 1, 
+            'F': 4, 
+            'G': 2, 
+            'H': 4, 
+            'I': 1, 
+            'J': 8, 
+            'K': 5, 
+            'L': 1, 
+            'M': 3, 
+            'N': 1, 
+            'O': 1, 
+            'P': 3, 
+            'Q': 10, 
+            'R': 1, 
+            'S': 1, 
+            'T': 1, 
+            'U': 1, 
+            'V': 4, 
+            'W': 4, 
+            'X': 8, 
+            'Y': 4, 
+            'Z': 10
     }
-
     score = 0
     word = word.upper()
-    for letter in word:
-        for number, letters in scores.items():
-            if letter in letters:
-                score += number
-    if len(word) > 6:
+    for i in word:
+        if i in scores:
+            score +=scores[i]
+    
+    if len(word) > 6 and len(word)<=10:
         score+=8
     return score
+          
+# def score_word(word):
+#     scores = { 
+#         1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+#         2: ["D", "G" ], 
+#         3: ["B", "C", "M", "P" ], 
+#         4: ["F", "H", "V", "W", "Y" ], 
+#         5: ["K"], 
+#         8: ["J", "X"],
+#         10: ["Q", "Z" ] 
+#     }
+
+#     score = 0
+#     word = word.upper()
+#     for letter in word:
+#         for number, letters in scores.items():
+#             if letter in letters:
+#                 score += number
+#     if len(word) > 6:
+#         score+=8
+#     return score
 
 
 def get_highest_word_score(word_list):
